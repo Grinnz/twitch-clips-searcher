62a255cf38e937bf1d82f3ba2ff5a9283d6c1f25
Dan Book <grinnz@grinnz.com>
2022-12-24 05:31:13 -0500

    clip filtering options

--- public/search.js
+++ public/search.js
@@ -14,16 +14,48 @@ createApp({
       clips: null,
       current_page: 1,
       clips_user: null,
+      title_search: null,
+      views_min: null,
+      views_max: null,
+      clips_filter_category: "",
+      clips_filter_clipper: "",
     }
   },
   computed: {
+    clips_filtered() {
+      if (this.clips === null) { return []; }
+      let filtered = this.clips;
+      if (this.clips_filter_category !== null && this.clips_filter_category !== "") {
+        filtered = filtered.filter(clip => this.clips_filter_category === clip.game);
+      }
+      if (this.clips_filter_clipper !== null && this.clips_filter_clipper !== "") {
+        filtered = filtered.filter(clip => this.clips_filter_clipper === clip.creator_name);
+      }
+      if ((this.views_min !== null && this.views_min !== "") || (this.views_max !== null && this.views_max !== "")) {
+        let min = this.views_min === "" ? null : this.views_min;
+        let max = this.views_max === "" ? null : this.views_max;
+        filtered = filtered.filter(clip => (min === null || clip.view_count >= min) && (max === null || clip.view_count <= max));
+      }
+      if (this.title_search !== null && this.title_search !== "") {
+        filtered = filtered.filter(clip => clip.title.toLowerCase().includes(this.title_search.toLowerCase()));
+      }
+      return filtered;
+    },
     clips_page() {
       if (this.clips === null) { return []; }
-      return this.clips.slice((this.current_page - 1) * this.page_size, this.current_page * this.page_size);
+      return this.clips_filtered.slice((this.current_page - 1) * this.page_size, this.current_page * this.page_size);
+    },
+    clip_categories() {
+      if (this.clips === null) { return []; }
+      return [...new Set(this.clips.map(clip => clip.game))].sort();
+    },
+    clip_clippers() {
+      if (this.clips === null) { return []; }
+      return [...new Set(this.clips.map(clip => clip.creator_name))].sort();
     },
     page_nums() {
       if (this.clips === null) { return []; }
-      return Array.from(new Array(Math.floor((this.clips.length - 1) / this.page_size) + 1), (x, i) => i + 1);
+      return Array.from(new Array(Math.floor((this.clips_filtered.length - 1) / this.page_size) + 1), (x, i) => i + 1);
     },
     pagination_prev_class() {
       return {
@@ -66,9 +98,8 @@ createApp({
         .then((response) => {
           if (response.ok) {
             return response.json().then((data) => {
-              this.current_page = 1;
-              this.clips = data.clips;
               this.clips_user = data.username;
+              this.populate_clips(data.clips);
             });
           } else {
             return response.json().then((data) => {
@@ -82,6 +113,15 @@ createApp({
           this.search_error = 'Internal error';
           console.log(error);
         }).finally(() => { this.fetching_clips = false; });
-    }
+    },
+    populate_clips(clips) {
+      this.current_page = 1;
+      this.title_search = null;
+      this.views_min = null;
+      this.views_max = null;
+      this.clips_filter_category = "";
+      this.clips_filter_clipper = "";
+      this.clips = clips;
+    },
   }
 }).mount('#app');
